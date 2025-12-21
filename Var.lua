ddb52ba3d720cbd82a7b03a1a19d14569c962dcc
x00182000 <deanjaylord218@gmail.com>
2025-12-21 18:21:14 +0800

    Update Var.lua

--- Var.lua
+++ Var.lua
@@ -1,5 +1,5 @@
 local Games = {
-  [7436755782] = "https://raw.githubusercontent.com/x00182000/x02182000GameScript/refs/heads/main/Grow%20a%20Garden/IncognitoHub.lua"
+  [7436755782] = "https://raw.githubusercontent.com/x00182000/x02182000GameScript/main/Grow%20a%20Garden/IncognitoHub.lua"
 }
 
 return Games
