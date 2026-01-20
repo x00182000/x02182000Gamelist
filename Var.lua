3eed789eaaa06317bd8507114a826d00f277f723
x00182000 <deanjaylord218@gmail.com>
2026-01-20 18:22:16 +0800

    Update Var.lua

--- Var.lua
+++ Var.lua
@@ -1,5 +1,6 @@
 local Games = {
-  [7436755782] = "https://raw.githubusercontent.com/x00182000/x02182000GameScript/main/Grow%20a%20Garden/IncognitoHub.lua"
+  [7436755782] = "https://raw.githubusercontent.com/x00182000/x02182000GameScript/main/Grow%20a%20Garden/IncognitoHub.lua",
+  [131623223084840] = "https://raw.githubusercontent.com/x00182000/x02182000GameScript/main/Escape%20Tsunami%20For%20Brainrots/Main.lua"
 }
 
 return Games
